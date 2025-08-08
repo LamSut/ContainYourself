84ad0ca08b7dedb9eba9695fac03b3c912449830
Truong Dang Truc Lam <116415510+LamSut@users.noreply.github.com>
2025-08-08 16:55:18 +0700

    Update ansible.sh

--- compose/pizza/ansible.sh
+++ compose/pizza/ansible.sh
@@ -17,29 +17,29 @@ else
   echo "Cloning the repository..."
   git clone "$REPO_URL" "$REPO_DIR"
   cd "$REPO_DIR" || { echo "Failed to enter repo directory"; exit 1; }
-fi
 
-# Copy config files into repo
-echo "Copying config files into repo..."
-cp -r ../config/. "$REPO_DIR"
-
-# Generate SSL certificates
-echo "Generating SSL certificates..."
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-  -keyout ./ssl/server.key -out ./ssl/server.crt \
-  -subj "/C=VN/ST=CT/L=NK/O=CTU/OU=CICT/CN=w0rm.org" \
-  -addext "basicConstraints=CA:FALSE"
-
-# Get public IP
-PUBLIC_IP=$(curl -s ifconfig.me)
-echo "Public IP: $PUBLIC_IP"
-
-# Comment this for local
-sed -i "s/localhost/$PUBLIC_IP/g" ./nginx.conf
-if grep -q "SWAGGER_HOST=" "$COMPOSE_FILE"; then
-  sed -i "s/SWAGGER_HOST=.*/SWAGGER_HOST=$PUBLIC_IP/" "$COMPOSE_FILE"
-else
-  echo "SWAGGER_HOST=$PUBLIC_IP" >> "$COMPOSE_FILE"
+  # Copy config files into repo
+  echo "Copying config files into repo..."
+  cp -r ../config/. "$REPO_DIR"
+  
+  # Generate SSL certificates
+  echo "Generating SSL certificates..."
+  openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout ./ssl/server.key -out ./ssl/server.crt \
+    -subj "/C=VN/ST=CT/L=NK/O=CTU/OU=CICT/CN=w0rm.org" \
+    -addext "basicConstraints=CA:FALSE"
+  
+  # Get public IP
+  PUBLIC_IP=$(curl -s ifconfig.me)
+  echo "Public IP: $PUBLIC_IP"
+  
+  # Comment this for local
+  sed -i "s/localhost/$PUBLIC_IP/g" ./nginx.conf
+  if grep -q "SWAGGER_HOST=" "$COMPOSE_FILE"; then
+    sed -i "s/SWAGGER_HOST=.*/SWAGGER_HOST=$PUBLIC_IP/" "$COMPOSE_FILE"
+  else
+    echo "SWAGGER_HOST=$PUBLIC_IP" >> "$COMPOSE_FILE"
+  fi
 fi
 
 echo "Building and starting Docker containers..."
